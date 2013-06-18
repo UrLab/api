6de2532f31ec8052ac8bf9a8074b17b4cb873cd3
etnarek <romain-fontaine@outlook.com>
2013-06-19 00:19:13 +0200

    pamela models

--- pamela/models.py
+++ pamela/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class stat(models.Model):
+	time = models.DateTimeField()
+	nbComputerUp = models.IntegerField()
+
+class userNames(models.Model):
+	name = models.CharField(max_length=200)
